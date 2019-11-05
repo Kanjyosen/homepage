fd9087ea350071a8bb7718ae96fffc9a7ba13d05
Zerosaka Makoto <Kanjyosen0@gmail.com>
2019-11-06 00:51:03 +0900

    💡

---
+++ docs/scripts/light_switch.js
@@ -0,0 +1,11 @@
+function switchlight(divid){
+    var base = document.getElementById(divid);
+    base.style.color= '#ffffff';
+    base.style.backgroundColor= '#16160e';
+}
+
+function turnonlight(divid){
+    var base = document.getElementById(divid);
+    base.style.color= '#000000';
+    base.style.backgroundColor= '#ffffff';
+}
