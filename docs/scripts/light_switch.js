7d091c14974b6346c66a59beca9c451df82c37bc
ZerosakaMakoto <kanjyosen0@gmail.com>
2021-05-29 02:45:55 +0900

    2021/05/28 26:46 ◆クエリによるダークモード状態引き継ぎ機能試験中◆

--- docs/scripts/light_switch.js
+++ docs/scripts/light_switch.js
@@ -1,11 +1,57 @@
+var switcher = "none";
+var querys = [];
+
+window.onload = function(){
+	querys = searchquery();
+	for(c = 0;c < querys.length;c++){
+		if(querys[c][0] == "light"){
+			switcher = querys[c][1];
+		}else{
+			/*なんもしてやんねーだ。*/
+		}
+	}
+	if(switcher == "true"){
+		turnonlight('pagebase');
+		turnonlight('mainpageL');
+	}else if(switcher == "false"){
+		switchlight('pagebase');
+		switchlight('mainpageL');
+	}else{
+		/*なんもせえへんで。*/
+	}
+}
+
+function searchquery(){
+	var fullparamete;
+	var splitedparam = [];
+	var splitedparam2 = [];
+	fullparamete = location.search.replace("?", "");
+	
+	if (fullparamete.indexOf("&") == -1){
+		splitedparam.push(fullparamete);
+	}else{
+		splitedparam = fullparamete.split("&");
+	}
+	for(c = 0;c < splitedparam.length;c++){
+		splitedparam[c] = splitedparam[c].split("=");
+	}
+	return splitedparam;
+}
+
 function switchlight(divid){
     var base = document.getElementById(divid);
     base.style.color= '#ffffff';
     base.style.backgroundColor= '#16160e';
+    var meare = base.innerHTML;
+    var sumire = meare.replace("?light=true", "?light=false");
+    base.innerHTML = sumire;
 }
 
 function turnonlight(divid){
     var base = document.getElementById(divid);
     base.style.color= '#000000';
     base.style.backgroundColor= '#ffffff';
+    var meare = base.innerHTML;
+    sumire = meare.replace("?light=false", "?light=true");
+    base.innerHTML = sumire;
 }
