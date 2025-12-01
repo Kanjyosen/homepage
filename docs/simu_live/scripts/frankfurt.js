95e8e1733b451847d3facb55a1b128321660b6da
Zerosaka Makoto <kanjyosen0@gmail.com>
2025-12-02 00:53:14 +0900

    added simu_live

---
+++ docs/simu_live/scripts/frankfurt.js
@@ -0,0 +1,68 @@
+let points = [];
+
+const CR = String.fromCharCode(13);
+const LF = String.fromCharCode(10);
+
+let shotImg = document.getElementById('shotImg');
+let placeTxt = document.getElementById('whereClicked');
+
+let pinID = '';
+let mapID = '';
+let placeName = '';
+
+function pin(event){
+  console.log(event.offsetX);
+  console.log(event.offsetY);
+  clickPtn = [event.offsetX, event.offsetY]
+  if (points.length == 0){
+    alert('座標カセットが未挿入です．');
+  }else{
+    for (let i = 0;i < points.length;i++){
+      if(clickPtn[0] <= points[i][1] && clickPtn[1] <= points[i][2] && clickPtn[0] >= points[i][3] && clickPtn[1] >= points[i][4]){
+        pinID = points[i][0];
+        placeName = points[i][5];
+      }
+    }
+    if(pinID != ''){
+      shotImg.src = ('./mapdata/' + mapID + '/' + pinID + '.png');
+      placeTxt.innerHTML = placeName;
+    }
+  }
+}
+
+const slot = document.getElementById('cassettePos_fileInput');
+
+function reqListener() {
+  console.log(this.responseText);
+}
+
+const req = new XMLHttpRequest();
+req.addEventListener('load', reqListener);
+req.open('GET', './mapdata/wakekoh.csv');
+req.send();
+
+function readcassette() {
+  const reader = new FileReader();
+  reader.addEventListener(
+    "load",
+    () => {
+      const rawTxt = reader.result;
+      const rowTxt = rawTxt.split(CR + LF);
+      
+      for (let i = 0;i < rowTxt.length;i++){
+        const colTxt = rowTxt[i].split(',');
+        points.push(colTxt);
+      }
+      
+      console.log(rowTxt);
+      console.log(points);
+    },
+    false,
+  );
+  
+  mapID = slot.files[0].name.slice(0, -4);
+  reader.readAsText(slot.files[0]);
+}
+
+let mapImg = document.getElementById('mapImg');
+mapImg.addEventListener('click', pin);
